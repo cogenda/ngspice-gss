701e819ac6e990c96af3a6938053d78e6cae3440
stefanjones <stefanjones>
2003-06-26 10:05:39 +0000

    Modified Files:     Makefile.am Added Files: 	cmpp/Makefile cmpp/cmpp.h cmpp/ifs_lex.l cmpp/ifs_yacc.h     cmpp/ifs_yacc.y cmpp/main.c cmpp/mod_lex.l cmpp/mod_yacc.h     cmpp/mod_yacc.y cmpp/pp_ifs.c cmpp/pp_lst.c cmpp/pp_mod.c     cmpp/read_ifs.c cmpp/util.c cmpp/writ_ifs.c
    Version of cmpp from xspice, still needs some work to make it compatable
    with SpiceOpus and tclspice, but compiles.

---
+++ src/xspice/cmpp/mod_yacc.h
@@ -0,0 +1,49 @@
+/* $Id: mod_yacc.h,v 1.1.2.1 2003/06/26 10:05:44 stefanjones Exp $  */
+
+/*============================================================================
+FILE  mod_yacc.h
+
+MEMBER OF process cmpp
+
+Copyright 1991
+Georgia Tech Research Corporation
+Atlanta, Georgia 30332
+All Rights Reserved
+
+PROJECT A-8503
+
+AUTHORS
+
+    9/12/91  Steve Tynor
+
+MODIFICATIONS
+
+    <date> <person name> <nature of modifications>
+
+SUMMARY
+
+    Typedefs needed by the YYSTYPE union (%union operator) in the yacc
+    file. These are only used in the yacc file, but must be defined here since
+    the generated token.h file includes a definition of the union YYSTYPE.
+
+INTERFACES
+
+    None.
+
+REFERENCED FILES
+
+    None.
+
+NON-STANDARD FEATURES
+
+    None.
+
+============================================================================*/
+
+#include "cmpp.h"
+
+typedef struct {
+   char *id;
+   Boolean_t has_subscript;
+   char *subscript;
+} Sub_Id_t;
